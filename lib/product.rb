fcfafbb5a9286e338de9c60c749ed20d8aa27ba2
Max Ribbans <ribbans.max@gmail.com>
2020-10-14 15:13:16 -0400

    Add name, unit_price, quantity, and total_price

--- lib/product.rb
+++ lib/product.rb
@@ -1,8 +1,14 @@
 class Product
-  attr_reader :category
-  
+  attr_reader :category, :name, :unit_price, :quantity
+
   def initialize(category, name, unit_price, quantity)
     @category = category
+    @name = name
+    @unit_price = unit_price
+    @quantity = quantity.to_i
+  end
 
+  def total_price
+    @quantity.to_f * @unit_price
   end
 end
