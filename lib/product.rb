f30224e2cbce8264296df0d7916d457bd660b9df
Max Ribbans <ribbans.max@gmail.com>
2020-10-14 15:18:39 -0400

    Add Iteration 1

--- lib/product.rb
+++ lib/product.rb
@@ -1,14 +1,23 @@
 class Product
-  attr_reader :category, :name, :unit_price, :quantity
+  attr_reader :category, :name, :unit_price, :quantity, :is_hoarded
 
-  def initialize(category, name, unit_price, quantity)
+  def initialize(category, name, unit_price, quantity, is_hoarded = false)
     @category = category
     @name = name
     @unit_price = unit_price
     @quantity = quantity.to_i
+    @is_hoarded = is_hoarded
   end
 
   def total_price
     @quantity.to_f * @unit_price
+  end
+
+  def is_hoarded?
+    is_hoarded
+  end
+
+  def hoard
+    @is_hoarded = true
   end
 end
--- test/product_test.rb
+++ test/product_test.rb
@@ -36,4 +36,15 @@ class ProductTest < Minitest::Test
     assert_equal 37.0, product1.total_price
     assert_equal 9.0, product2.total_price
   end
+
+  def test_it_is_not_hoarded_by_default
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    assert_equal false, product1.is_hoarded?
+  end
+
+  def test_can_be_hoarded
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product1.hoard
+    assert_equal true, product1.is_hoarded?
+  end
 end
